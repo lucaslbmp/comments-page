d11157aecf2f795524ca9a245d63ee7d1be90a14
Lucas <lucaslbmp@github.com>
2024-09-12 17:39:42 -0300

    Adding danger variant to button

--- src/app/_components/button.tsx
+++ src/app/_components/button.tsx
@@ -1,23 +1,26 @@
+import { twMerge } from "tailwind-merge";
+
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: BtnVariantType;
 }
 
-type BtnVariantType = "primary" | "secondary";
+type BtnVariantType = "primary" | "secondary" | "danger";
 
 const variantMap: Record<BtnVariantType, string> = {
   primary: "bg-primary text-white ",
   secondary: "bg-fgPrimary text-white ",
+  danger: "bg-danger text-white ",
 };
 
 const Button = (props: ButtonProps) => {
   return (
     <button
       {...props}
-      className={
+      className={twMerge(
         variantMap[props.variant ?? "primary"] +
-        " p-2 rounded-md uppercase w-[5rem] hover:opacity-70 " +
+          " p-2 rounded-md uppercase w-[5rem] hover:opacity-70 ",
         props.className
-      }
+      )}
     ></button>
   );
 };
